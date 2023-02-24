1d82985f74e8358dd9928a0cd16e9a58653aaca5
HyunJun123456 <juni303@chauniv.ac.kr>
2023-02-24 21:08:33 +0900

    회원가입완료 및 패스워드 해시 구조

--- src/main/java/com/cos/photogramstart/web/AuthController.java
+++ src/main/java/com/cos/photogramstart/web/AuthController.java
@@ -2,17 +2,23 @@ package com.cos.photogramstart.web;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import com.cos.photogramstart.domain.user.User;
+import com.cos.photogramstart.service.AuthService;
 import com.cos.photogramstart.web.dto.auth.SignupDto;
 
+// @RequiredArgsConstructor // final 필드를 DI 할때 사용
 @Controller // 1. IoC 2. 파일을 리턴하는 컨트롤러
 public class AuthController {
 	
 	private static final Logger log = LoggerFactory.getLogger(AuthController.class);
-
+	
+	@Autowired // 의존성 주입
+	private AuthService authService;
 	
 	@GetMapping("/auth/signin")
 	public String signinForm() {
@@ -34,6 +40,11 @@ public class AuthController {
 	@PostMapping("/auth/signup")
 	public String signup(SignupDto signupDto) { // key=value (x-www-form-urlencoded)
 		log.info(signupDto.toString());
+		// User <- SignupDto
+		User user = signupDto.toEntity();
+		log.info(user.toString());
+		User userEntity = authService.회원가입(user);
+		System.out.println(userEntity);
 		return "auth/signin"; // 회원가입 성공시
 	}
 	
