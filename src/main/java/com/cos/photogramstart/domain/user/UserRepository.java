b05af5e67cc6c5c27090e44307a1327cfaa7c46b
HyunJun123456 <juni303@chauniv.ac.kr>
2023-02-26 05:13:50 +0900

    로그인 완료 및 View 연결

--- src/main/java/com/cos/photogramstart/domain/user/UserRepository.java
+++ src/main/java/com/cos/photogramstart/domain/user/UserRepository.java
@@ -2,6 +2,7 @@ package com.cos.photogramstart.domain.user;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 // 어노테이션이 없어도 JpaRepository를 상속하면 IoC 등록이 자동으로 된다.
-public interface UserRepository extends JpaRepository<User, Integer>{
-
+public interface UserRepository extends JpaRepository<User, Integer>{ // User Entity의 ID가 int 타입임
+	// JPA query methods
+	User findByUsername(String username);
 }
