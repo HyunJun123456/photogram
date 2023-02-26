a6acb8e870e6399cd4a9b949fcfef79cb93533a4
HyunJun123456 <juni303@chauniv.ac.kr>
2023-02-26 22:43:42 +0900

    세션정보 확인 및 회원정보 수정

--- src/main/java/com/cos/photogramstart/web/UserController.java
+++ src/main/java/com/cos/photogramstart/web/UserController.java
@@ -1,8 +1,14 @@
 package com.cos.photogramstart.web;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+
+import com.cos.photogramstart.config.auth.PrincipalDetails;
 
 @Controller
 public class UserController {
@@ -12,7 +18,15 @@ public class UserController {
 	}
 	
 	@GetMapping("/user/{id}/update")
-	public String update(@PathVariable int id) {
+	public String update(@PathVariable int id, @AuthenticationPrincipal PrincipalDetails principalDetails) { // 그래서 어노테이션을 만들어줌
+		// session -> securityContextholder -> Authentication -> PrincipalDetails(UserDetails)
+		System.out.println("세션 정보: "+principalDetails.getUser());
+		/*
+		 * Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+		 * PrincipalDetails mPrincipalDetails = (PrincipalDetails) auth.getPrincipal();
+		 * System.out.println("직접 찾은 세션 정보: "+mPrincipalDetails.getUser());
+		 */
+		//model.addAttribute("principal", principalDetails.getUser());
 		return "user/update";
 	}
 	
