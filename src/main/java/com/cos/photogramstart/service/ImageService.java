d849c5d898e46fbfea4c0d72e153bb4f17a201af
HyunJun123456 <juni303@chauniv.ac.kr>
2023-03-01 00:51:20 +0900

    예외처리와 API 주소 시큐리티 설정. 프로필 페이지 1 (image 모델, 업로드)...end

--- src/main/java/com/cos/photogramstart/service/ImageService.java
+++ src/main/java/com/cos/photogramstart/service/ImageService.java
@@ -1,5 +1,11 @@
 package com.cos.photogramstart.service;
 
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.UUID;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import com.cos.photogramstart.config.auth.PrincipalDetails;
@@ -13,7 +19,23 @@ import lombok.RequiredArgsConstructor;
 public class ImageService {
 	
 	private final ImageRepository imageRepository;
+	
+	@Value("${file.path}")
+	private String uploadFolder;
+	
 	public void 사진업로드(ImageUploadDto imageUploadDto, PrincipalDetails principalDetails) {
+		UUID uuid = UUID.randomUUID(); // uuid
+		String imageFileName = uuid + "_" + imageUploadDto.getFile().getOriginalFilename();
+		System.out.println("이미지 파일이름: "+imageFileName);
+		
+		Path imageFilePath = Paths.get(uploadFolder+imageFileName);
+		
+		// 통신, I/O -> 예외가 발생할 수 있다.
+		try {
+			Files.write(imageFilePath, imageUploadDto.getFile().getBytes());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 		
 	}
 	
